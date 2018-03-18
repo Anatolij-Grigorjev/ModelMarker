9299036c4ae812656938a98fac15650f52f7c7b7
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2018-03-18 11:04:41 +0200

    Initial TornadoFX commit
    Signed-off-by: Anatolij Grigorjev <grigorjev.anatolij@gmail.com>

---
+++ src/main/kotlin/com/tiem625/modemarker/app/MyApp.kt
@@ -0,0 +1,6 @@
+package com.tiem625.modemarker.app
+
+import com.tiem625.modemarker.view.MainView
+import tornadofx.*
+
+class MyApp: App(MainView::class, Styles::class)
---
+++ src/main/kotlin/com/tiem625/modemarker/view/MainView.kt
@@ -0,0 +1,12 @@
+package com.tiem625.modemarker.view
+
+import com.tiem625.modemarker.app.Styles
+import tornadofx.*
+
+class MainView : View("Hello TornadoFX") {
+    override val root = hbox {
+        label(title) {
+            addClass(Styles.heading)
+        }
+    }
+}
