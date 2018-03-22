37d74cbd6a0e15986d560e415fd1a9c8d80a605f
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2018-03-22 20:53:12 +0200

    add initial border pane with upper menu
    Signed-off-by: Anatolij Grigorjev <grigorjev.anatolij@gmail.com>

--- src/main/kotlin/com/tiem625/modemarker/view/MainView.kt
+++ src/main/kotlin/com/tiem625/modemarker/view/MainView.kt
@@ -1,14 +1,50 @@
 package com.tiem625.modemarker.view
 
-import com.tiem625.modemarker.app.Styles
 import com.tiem625.modemarker.app.Version
+import javafx.application.Platform
+import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyCodeCombination
+import javafx.scene.input.KeyCombination
 import tornadofx.*
 
 class MainView : View("Model Marker v${Version.versionString} (tm)") {
 
-    override val root = hbox {
-        label(title) {
-            addClass(Styles.heading)
+    private fun mainPlus(keyCode: KeyCode): KeyCombination =
+            KeyCodeCombination(keyCode, KeyCodeCombination.SHORTCUT_DOWN)
+
+    override val root = borderpane {
+        //menu bar on the top
+        top {
+
+            useMaxWidth = true
+            menubar {
+                menu("File") {
+
+                    item("New", mainPlus(KeyCode.N))
+                    item("Open", mainPlus(KeyCode.O))
+                    item("Save", mainPlus(KeyCode.S))
+                    separator()
+                    item("Exit", mainPlus(KeyCode.Q)).setOnAction {
+
+                        Platform.exit()
+                    }
+                }
+            }
+        }
+
+        //image parameters on the left
+        left {
+
+        }
+
+        //status bar on the bottom
+        bottom {
+
+        }
+
+        //image grid in the center
+        center {
+
         }
     }
 }
