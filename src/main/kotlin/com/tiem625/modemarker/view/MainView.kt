d65f375c8a159bf2c03403ca9a0dc3cd638b3b27
Anatolij Grigorjev <anatolij@mediapark.lt>
2018-03-23 21:25:04 +0200

    expanding left pane of modelmaker borderpane

--- src/main/kotlin/com/tiem625/modemarker/view/MainView.kt
+++ src/main/kotlin/com/tiem625/modemarker/view/MainView.kt
@@ -2,6 +2,7 @@ package com.tiem625.modemarker.view
 
 import com.tiem625.modemarker.app.Version
 import javafx.application.Platform
+import javafx.geometry.Orientation
 import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyCodeCombination
 import javafx.scene.input.KeyCombination
@@ -34,7 +35,33 @@ class MainView : View("Model Marker v${Version.versionString} (tm)") {
 
         //image parameters on the left
         left {
+            form {
+                fieldset("Image Info", labelPosition = Orientation.VERTICAL) {
 
+                    field("Image Width:") {
+                        textfield {
+                            isEditable = false
+                        }
+                    }
+                    field ("Image Height:") {
+                        textfield {
+                            isEditable = false
+                        }
+                    }
+                    field("Spritesheet Rows:"){
+                        textfield()
+                    }
+                    field("Spritesheet Columns:") {
+                        textfield()
+                    }
+                    field("Sprite Padding X:") {
+                        textfield()
+                    }
+                    field("Sprite Padding Y:") {
+                        textfield()
+                    }
+                }
+            }
         }
 
         //status bar on the bottom
